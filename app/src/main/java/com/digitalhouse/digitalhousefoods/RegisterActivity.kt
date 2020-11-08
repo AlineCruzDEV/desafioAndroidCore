23343e9feaa50365f39b4be8014e1dbadb292d5d
AlineCruzDEV <linegcruz@gmail.com>
2020-11-08 14:04:16 -0300

    Evento de click no botao Register

--- app/src/main/java/com/digitalhouse/digitalhousefoods/RegisterActivity.kt
+++ app/src/main/java/com/digitalhouse/digitalhousefoods/RegisterActivity.kt
@@ -1,11 +1,24 @@
 package com.digitalhouse.digitalhousefoods
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        //Click in REGISTER Button
+        btRegister2.setOnClickListener{
+            callMain()
+        }
+    }
+
+    //Function that through the intent will start the HomePageActivity
+    private fun callMain() {
+        var intent = Intent(this, HomePageActivity::class.java)
+        startActivity(intent)
     }
 }
